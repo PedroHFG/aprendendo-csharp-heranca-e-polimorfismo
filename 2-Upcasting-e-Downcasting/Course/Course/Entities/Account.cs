f3cd326d88e2a7e4ff5afa7b408316029c12aec4
PedroHFG <pedrohfidg@gmail.com>
2024-12-28 15:19:24 -0300

    Upcasting e downcasting

---
+++ 2-Upcasting-e-Downcasting/Course/Course/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace Course.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; } // O modificador private no set indica que o atributo so pode ser modificado na propria classe
+        public double Balance { get; protected set; } // O modificador protected no set indica que o atributo pode ser modificado apenas na subclasse que herdar Account e não em outras classes
+
+        public Account() 
+        { 
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ 2-Upcasting-e-Downcasting/Course/Course/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Course.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance) 
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance() 
+        {
+            Balance +=  Balance * InterestRate;
+        }
+    }
+}
